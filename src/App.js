f03dbcb43ba7900359c633c4d67614dd62467de9
SwarajKaran <swarajkaran.raj@gmail.com>
2022-11-20 01:46:24 +0530

    GET & POST from firebase

--- src/App.js
+++ src/App.js
@@ -2,30 +2,49 @@ import React, { useEffect, useState, useCallback } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 import Loader from './components/Loader';
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-
+  const addMovieHandler = async (movie) => {
+    const response = await fetch(
+      'https://react-http-f9803-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  };
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      // const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch(
+        'https://react-http-f9803-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json'
+      );
       if (!response.ok) {
         throw new Error(`${response.status}: Something went wrong`);
       }
       const data = await response.json();
-      const transformedMovies = data.results.map((movie) => {
-        return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setMovies([]);
       setError(error.message);
@@ -47,6 +66,9 @@ function App() {
   }
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
