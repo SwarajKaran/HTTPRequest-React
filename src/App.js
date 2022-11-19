c243d34cf7fec7f5c8f144ef5b692ba6095b85e1
SwarajKaran <swarajkaran.raj@gmail.com>
2022-11-20 00:23:52 +0530

    Added Loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-
+import { PuffLoader } from 'react-spinners';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
     const transformedMovies = data.results.map((movie) => {
@@ -17,6 +19,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -25,7 +28,13 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No movies found</p>}
+        {isLoading && (
+          <p>
+            <PuffLoader color="#230052" />
+          </p>
+        )}
       </section>
     </React.Fragment>
   );
